3408d9c2661472b4d6695898629559c8202be8de
Daniel Weiss <daniel.weiss@innogames.com>
2020-12-10 19:58:23 +0100

    Day 10-2

--- src/main/kotlin/days/Day10.kt
+++ src/main/kotlin/days/Day10.kt
@@ -1,16 +1,17 @@
 package days
 
 import java.io.File
+import kotlin.math.min
 
 class Day10 : Day {
-    private val data = File("src/main/resources/data10.txt").useLines { it.toList().map { s -> s.toInt() } }
+    private val data = File("src/main/resources/data10.txt").useLines { it.toList().map { s -> s.toInt() } }.sorted()
 
     override fun executePart1(): Day {
         var start = 0
         var jolt1 = 0
         var jolt3 = 1 // last one
 
-        for (jolt in data.sorted()) {
+        for (jolt in data) {
             when (jolt-start) {
                 1 -> jolt1++
                 3 -> jolt3++
@@ -25,7 +26,31 @@ class Day10 : Day {
     }
 
     override fun executePart2(): Day {
-        println("Part 2: Missing")
+        val results = mutableMapOf<Int, Long>()
+        val list = listOf(0) + data + listOf(data.last()+3)
+
+        fun dp(i: Int): Long {
+            if (i == list.size-1) {
+                return 1
+            }
+
+            if (results.containsKey(i)) {
+                return results.getValue(i)
+            }
+
+            var total = 0L
+
+            for (j in i+1 until min(i+4, list.size)) {
+                if (list[j] - list[i] <= 3) {
+                    total += dp(j)
+                }
+            }
+
+            results[i] = total
+            return total
+        }
+
+        println("Part 2: " + dp(0))
 
         return this
     }
