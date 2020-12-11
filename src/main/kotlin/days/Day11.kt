42aecd6a60e75f12b7d5716b698f0ec3fcde2b03
Daniel Weiss <daniel.weiss@innogames.com>
2020-12-11 21:14:52 +0100

    Day 11-2

--- src/main/kotlin/days/Day11.kt
+++ src/main/kotlin/days/Day11.kt
@@ -14,7 +14,7 @@ class Day11 : Day {
     override fun executePart1(): Day {
 
         fun traverse(list: List<List<Position>>): Int {
-            val newList = applyRules(list)
+            val newList = applyRulesPart1(list)
 
             return if (newList != list) {
                 traverse(newList)
@@ -28,7 +28,7 @@ class Day11 : Day {
         return this
     }
 
-    private fun applyRules(data: List<List<Position>>): MutableList<MutableList<Position>> {
+    private fun applyRulesPart1(data: List<List<Position>>): MutableList<MutableList<Position>> {
         val newData: MutableList<MutableList<Position>> = mutableListOf()
 
         for (row in data.withIndex()) {
@@ -41,6 +41,27 @@ class Day11 : Day {
                         newData[row.index][cell.index].type = OCCUPIED
                     }
                     OCCUPIED -> if (getAdjacentOccupied(data, row.index, cell.index) >= 4) {
+                        newData[row.index][cell.index].type = EMPTY
+                    }
+                }
+            }
+        }
+        return newData
+    }
+
+    private fun applyRulesPart2(data: List<List<Position>>): MutableList<MutableList<Position>> {
+        val newData: MutableList<MutableList<Position>> = mutableListOf()
+
+        for (row in data.withIndex()) {
+            newData.add(row.index, mutableListOf())
+            for (cell in row.value.withIndex()) {
+                newData[row.index].add(cell.index, Position(data[row.index][cell.index].type))
+
+                when (cell.value.type) {
+                    EMPTY -> if (getVisibleOccupied(data, row.index, cell.index) == 0) {
+                        newData[row.index][cell.index].type = OCCUPIED
+                    }
+                    OCCUPIED -> if (getVisibleOccupied(data, row.index, cell.index) >= 5) {
                         newData[row.index][cell.index].type = EMPTY
                     }
                 }
@@ -73,7 +94,139 @@ class Day11 : Day {
         return total
     }
 
+    private fun getVisibleOccupied(data: List<List<Position>>, y: Int, x: Int): Int {
+        var total = 0
+
+        // N
+        var j = y
+        while (--j >= 0) {
+            when (data[j][x].type) {
+                OCCUPIED -> {
+//                    println("N -> X: $x, Y: $j")
+                    total++
+                    break
+                }
+                EMPTY -> break
+            }
+        }
+
+        // S
+        j = y
+        while (++j < data.size) {
+            when (data[j][x].type) {
+                OCCUPIED -> {
+//                    println("S -> X: $x, Y: $j")
+                    total++
+                    break
+                }
+                EMPTY -> break
+            }
+        }
+
+        // W
+        var i = x
+        while (--i >= 0) {
+            when (data[y][i].type) {
+                OCCUPIED -> {
+//                    println("W -> X: $i, Y: $y")
+                    total++
+                    break
+                }
+                EMPTY -> break
+            }
+        }
+
+        // E
+        i = x
+        while (++i < data[0].size) {
+            when (data[y][i].type) {
+                OCCUPIED -> {
+//                    println("E -> X: $i, Y: $y")
+                    total++
+                    break
+                }
+                EMPTY -> break
+            }
+        }
+
+        // NW
+        j = y
+        i = x
+        while (--j >= 0 && --i >= 0) {
+            when (data[j][i].type) {
+                OCCUPIED -> {
+//                    println("NW -> X: $i, Y: $j")
+                    total++
+                    break
+                }
+                EMPTY -> break
+            }
+        }
+
+        // NE
+        j = y
+        i = x
+        while (--j >= 0 && ++i < data[0].size) {
+            when (data[j][i].type) {
+                OCCUPIED -> {
+//                    println("NE -> X: $i, Y: $j")
+                    total++
+                    break
+                }
+                EMPTY -> break
+            }
+        }
+
+        // SW
+        j = y
+        i = x
+        while (++j < data.size && --i >= 0) {
+            when (data[j][i].type) {
+                OCCUPIED -> {
+//                    println("SW -> X: $i, Y: $j")
+                    total++
+                    break
+                }
+                EMPTY -> break
+            }
+        }
+
+        // SE
+        j = y
+        i = x
+        while (++j < data.size && ++i < data[0].size) {
+            when (data[j][i].type) {
+                OCCUPIED -> {
+//                    println("SE -> X: $i, Y: $j")
+                    total++
+                    break
+                }
+                EMPTY -> break
+            }
+        }
+
+        return total
+    }
+
     override fun executePart2(): Day {
-        TODO("Not yet implemented")
+        fun traverse(list: List<List<Position>>): Int {
+//            list.forEach { l ->
+//                println()
+//                l.forEach{ print(it.type)}
+//            }
+//            println("\n============")
+
+            val newList = applyRulesPart2(list)
+
+            return if (newList != list) {
+                traverse(newList)
+            } else {
+                newList.map { mutableList -> mutableList.count { position -> position.type == OCCUPIED } }.sum()
+            }
+        }
+
+        println("Part 2: " + traverse(data))
+
+        return this
     }
 }
