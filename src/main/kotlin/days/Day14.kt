3f4934ff39ad1339b69c5c48ee2661d61cebf134
Daniel Weiss <daniel.weiss@innogames.com>
2020-12-14 13:49:34 +0100

    Day 14-2

--- src/main/kotlin/days/Day14.kt
+++ src/main/kotlin/days/Day14.kt
@@ -41,6 +41,45 @@ class Day14 : Day {
     }
 
     override fun executePart2(): Day {
-        TODO("Not yet implemented")
+        var mask = ""
+        val results = mutableMapOf<Long, Long>()
+
+        data.forEach { line ->
+            if (line.contains("mask")) {
+                mask = line.split("=").last().trim()
+            } else {
+                val (_, address, value) = "\\[(\\d*)\\] = (\\d*)".toRegex().find(line)?.groupValues!!
+
+                fun replace(b: String, index: Int) {
+                    if (index == mask.length) {
+                        var a = 0.0
+                        b.reversed().forEachIndexed { i, c ->
+                            if (c == '1') {
+                                a += 2.0.pow(i)
+                            }
+                        }
+
+                        results[a.toLong()] = value.toLong()
+
+                        return
+                    }
+
+                    when (val c = mask[index]) {
+                        '1' -> replace(b.replaceRange(index, index+1, c.toString()), index + 1)
+                        '0' -> replace(b, index + 1)
+                        'X' -> {
+                            replace(b.replaceRange(index, index+1, "1"), index + 1)
+                            replace(b.replaceRange(index, index+1, "0"), index + 1)
+                        }
+                    }
+                }
+
+                val binary = Integer.toBinaryString(address.toInt()).padStart(mask.length, '0')
+                replace(binary, 0)
+            }
+        }
+        println("Part 2: " + results.values.sum())
+
+        return this
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,23 +2,23 @@ import days.*
 
 fun main() {
     listOf(
-//        Day1(),
-//        Day2(),
-//        Day3(),
-//        Day4(),
-//        Day5(),
-//        Day6(),
-//        Day7(),
-//        Day8(),
-//        Day9(),
-//        Day10(),
-//        Day11(),
-//        Day12(),
-//        Day13(),
+        Day1(),
+        Day2(),
+        Day3(),
+        Day4(),
+        Day5(),
+        Day6(),
+        Day7(),
+        Day8(),
+        Day9(),
+        Day10(),
+        Day11(),
+        Day12(),
+        Day13(),
         Day14()
     ).forEach { it
         .printHeader()
         .executePart1()
-//        .executePart2()
+        .executePart2()
     }
 }
